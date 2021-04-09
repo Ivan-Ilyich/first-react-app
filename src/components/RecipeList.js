146874ae512f6c72cd85cc1c21760a8cef548b69
IvanIlyich3 <chpechlivanidis@gmail.com>
2021-04-09 19:20:42 +0300

    build basic CSS

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -4,7 +4,7 @@ import Recipe from './Recipe'
 export default function RecipeList({ recipes }) {
     console.log(recipes);
     return (
-        <>
+        <div className="recipe-list">
             <div>
                 {recipes.map(recipe => {
                     return (
@@ -15,7 +15,9 @@ export default function RecipeList({ recipes }) {
                     )
                 })}
             </div>
-            <button>Add Recipe</button>
-        </>
+            <div className="recipe-list__add-recipe-btn-container">
+                <button className="btn btn--primary">Add Recipe</button>
+            </div>
+        </div>
     )
 }
