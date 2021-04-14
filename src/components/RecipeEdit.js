43fb9763073523b0707cf6577ca7c3ea6c2fc16d
IvanIlyich3 <chpechlivanidis@gmail.com>
2021-04-14 12:32:47 +0300

    Build CSS for RecipeEdit and RecipeIngredientEdit components

--- src/components/RecipeEdit.js
+++ src/components/RecipeEdit.js
@@ -4,30 +4,70 @@ import RecipeIngredientEdit from './RecipeIngredientEdit';
 export default function RecipeEdit() {
     return (
         <div className="recipe-edit">
-            <div>
-                <button>&times;</button>
+            <div className="recipe-edit__remove-button-container">
+                <button className="btn recipe-edit__remove-button">&times;</button>
             </div>
-            <div>
-                <label htmlFor="name">Name</label>
-                <input type="text" id="name" name="name" />
-                <label htmlFor="cookTime">Cook Time</label>
-                <input type="text" id="cookTime" name="cookTime" />
-                <label htmlFor="servings">Servings</label>
-                <input type="text" id="servings" name="servings" min="1" />
-                <label htmlFor="instructions">Instructions</label>
-                <textarea id="instructions" name="instructions" />
+            <div className="recipe-edit__details-grid">
+                <label 
+                    htmlFor="name" 
+                    className="recipe-edit__label"
+                >
+                    Name
+                </label>
+                <input 
+                    type="text" 
+                    id="name" 
+                    name="name" 
+                    className="recipe-edit__input" 
+                />
+                <label 
+                    htmlFor="cookTime" 
+                    className="recipe-edit__label"
+                >        
+                    Cook Time
+                </label>
+                <input 
+                    type="text" 
+                    id="cookTime" 
+                    name="cookTime" 
+                    className="recipe-edit__input" 
+                />
+                <label 
+                    htmlFor="servings" 
+                    className="recipe-edit__label"
+                >
+                    Servings
+                </label>
+                <input 
+                    type="number" 
+                    id="servings" 
+                    name="servings" 
+                    className="recipe-edit__input" 
+                    min="1" 
+                />
+                <label 
+                    htmlFor="instructions" 
+                    className="recipe-edit__label"
+                >
+                    Instructions
+                </label>
+                <textarea 
+                    id="instructions" 
+                    name="instructions"
+                    className="recipe-edit__input" 
+                />
             </div>
             <br />
-            <label>Ingredients</label>
-            <div>
+            <label  className="recipe-edit__label">Ingredients</label>
+            <div className="recipe-edit__ingredient-grid">
                 <div>Name</div>
                 <div>Amount</div>
                 <div></div>
                 <RecipeIngredientEdit />
                 <RecipeIngredientEdit />
             </div>
-            <div>
-                <button>Add Ingredient</button>
+            <div className="recipe-edit__add-ingredient-button-container">
+                <button className="btn btn--primary">Add Ingredient</button>
             </div>
         </div>
         
